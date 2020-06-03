5ff90c6b51e2e9dfcd0ce3106d6af3d09f377052
kwennB <60541622+kwennB@users.noreply.github.com>
2020-06-03 02:49:26 +0100

    first commit

---
+++ kwennb-HNG-01909.js
@@ -0,0 +1,6 @@
+let fullName = "Anyebe Blessing Ene Omotayo";
+let hngId = "01909";
+let language = "javascript"
+
+
+console.log(`Hello  World, this is ${fullName} with HNG ID ${hngId} using ${language} for stage 2 task`);
